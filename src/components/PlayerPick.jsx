2db5dd66f86139b7833371f49aef17f7303dc92c
marcmarant <marcosmardev@gmail.com>
2023-08-22 21:39:56 +0200

    Solucion problema id's

--- src/components/PlayerPick.jsx
+++ src/components/PlayerPick.jsx
@@ -10,7 +10,12 @@ export const PlayerPick = ( {onSelect} ) => { // onSelect es llamado cuando <sel
         const getPlayers = async () => {
             try {
                 const snapshot = await getDocs(playersCollection)
-                const playersArray = snapshot.docs.map((doc) => doc.data().mote)
+                const playersArray = snapshot.docs.map((doc) => {
+                    return {
+                        'id': doc.id,
+                        'mote': doc.data().mote
+                    }
+                })
                 setPlayers(playersArray)
             }
             catch(error) {
@@ -22,11 +27,11 @@ export const PlayerPick = ( {onSelect} ) => { // onSelect es llamado cuando <sel
 
     return (
         <div>
-            <select onChange={(e) => onSelect(e.target.value)}>
+            <select onChange={(e) => onSelect(e.target.options[e.target.selectedIndex].getAttribute('data-key'))}>
                 <option disabled selected value="">Seleccione un jugador</option> // selected no es optimo
                 {players.map((player) => (
-                    <option key={player}>
-                        {player}
+                    <option key={player.mote} data-key={player.id}>
+                        {player.mote}
                     </option>
                 ))}
             </select>
--- src/scripts/algoritmoElo.js
+++ src/scripts/algoritmoElo.js
@@ -17,11 +17,11 @@ export function algoritmoElo(partido) {
     const eVic = Math.floor((partido.defensaVic.data.defensa + partido.delanteroVic.data.ataque)/2);
     const eDerr = Math.floor((partido.defensaDerr.data.defensa + partido.delanteroDerr.data.ataque)/2);
     // Porcentaje de victoria de la pareja vencedora/perdedora
-    const pVic = Math.floor((1+10^((eDerr-eVic)/400))*100)/100
-    const pDerr = Math.floor((1+10^((eDerr-eVic)/400))*100)/100
+    const pVic = Math.floor((1+10^((eDerr-eVic)/400))*100)/100;
+    const pDerr = Math.floor((1+10^((eDerr-eVic)/400))*100)/100;
     // Elo ganado/perdido por la pareja vencedora/perdedora (facor de multiplicacion 32)
-    const wVic = 32*(1-pVic)
-    const wDerr = 32*(-pDerr)
+    const wVic = 32*(1-pVic);
+    const wDerr = 32*(-pDerr);
 
     // Se suma el elo ganado/perdido a todos los jugadores en su respectiva posicion
     partido.delanteroVic.data.ataque += wVic;
