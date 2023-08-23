2ba18d921232ae4ccff21d493b7317b1d5fb8007
marcmarant <marcosmardev@gmail.com>
2023-08-23 15:49:29 +0200

    Algoritmo ELO errores solucionados

--- src/scripts/algoritmoElo.js
+++ src/scripts/algoritmoElo.js
@@ -16,12 +16,12 @@ export function algoritmoElo(partido) {
     // Elo medio pareja vencedora/perdedora
     const eVic = Math.floor((partido.defensaVic.data.defensa + partido.delanteroVic.data.ataque)/2);
     const eDerr = Math.floor((partido.defensaDerr.data.defensa + partido.delanteroDerr.data.ataque)/2);
-    // Porcentaje de victoria de la pareja vencedora/perdedora
-    const pVic = Math.floor((1+10^((eDerr-eVic)/400))*100)/100;
-    const pDerr = Math.floor((1+10^((eDerr-eVic)/400))*100)/100;
-    // Elo ganado/perdido por la pareja vencedora/perdedora (facor de multiplicacion 32)
-    const wVic = 32*(1-pVic);
-    const wDerr = 32*(-pDerr);
+    // Porcentaje de victoria de la pareja vencedora/perdedora7
+    const pVic = (1/(1+Math.pow(10, (eDerr-eVic)/600))).toFixed(3)
+    const pDerr = (1-pVic).toFixed(3);
+    // Elo ganado/perdido por la pareja vencedora/perdedora (factor de multiplicacion 3, factor de crecimiento 6)
+    const wVic = Math.ceil(Math.pow(3*(pDerr), 6));
+    const wDerr = -wVic;
 
     // Se suma el elo ganado/perdido a todos los jugadores en su respectiva posicion
     partido.delanteroVic.data.ataque += wVic;
