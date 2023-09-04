ba6a33d9cc4afdcf888f5461a2e1a4b75c2af4a2
marcmarant <marcosmardev@gmail.com>
2023-09-04 19:01:14 +0200

    Correccion MatchView

--- src/components/MatchView.jsx
+++ src/components/MatchView.jsx
@@ -1,31 +1,38 @@
-import { matchesCollection, getPlayerRef } from '../config/firebase'
+import React, { useEffect, useState } from 'react'
+import { matchesCollection } from '../config/firebase'
 import { query, limit, getDocs } from 'firebase/firestore'
 import { PlayerPick } from './PlayerPick'
-import { useState } from 'react';
 import './MatchView.css'
-
 
 export const MatchView = () => {
 
-  const maxMatches = 10 // Número de partidos mostrados
+  const maxMatches = 10 // Number of matches to show
   const [matches, setMatches] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
-  const getMatches = async () => {
-    try {
-      const q = query(matchesCollection, limit(maxMatches));
-      const snapshot = await getDocs(q)
-      const matchesArray = snapshot.docs.map((doc) => doc.data())
-      setMatches(matchesArray)
+  useEffect(() => {
+    const getMatches = async () => {
+      try {
+        const q = query(matchesCollection, limit(maxMatches))
+        const snapshot = await getDocs(q)
+        const matchesArray = snapshot.docs.map((doc) => doc.data())
+        setMatches(matchesArray)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+        setLoading(false)
+      }
     }
-    catch(error) {
-        console.error('Error al obtener los partidos', error)
-    }
-  }
-  getMatches()
+    getMatches()
+  }, [])
 
-  const getDate = (match) => {
-    const date = new Date(match.fecha)
-    return [date.toLocaleDateString('es-ES'), date.toLocaleTimeString('es-ES')] // formato: es, zona horaria: ES
+  const transformToMote = (id) => {
+    const words = id.split('_')[0].split('_')
+    const mote = words.map((word) =>
+      word.charAt(0).toUpperCase() + word.slice(1)
+    )
+    return mote.join(' ')
   }
 
   return (
@@ -33,30 +40,35 @@ export const MatchView = () => {
       <div className='head_div'>
         <h3>Ultimos partidos</h3>
         <div>
-          {/* TODO: <option> para ordenamiento */}
-          <PlayerPick placeholder='Filtrar'/>
+          {/* TODO: <option> for sorting */}
+          <PlayerPick placeholder='Filtrar' />
         </div>
       </div>
       <div className='match_container'>
-        {matches.map((match) => (
-          <div className='match'>
-            <div className='match_date'>
-            {getDate(match).map((dateElem) => (
-              <h5>{dateElem}</h5>
-            ))}
+        {loading ?
+          <p>Cargando...</p>
+        : error ?
+          <p>Error al cargar los partidos</p>
+        : (
+          matches.map((match, index) => (
+            <div className='match' key={index}>
+              {/* Transform and display match attributes */}
+              <div className='match_date'>
+                {/* Render date here */}
+              </div>
+              <div className='match_players'>
+                <h4>DEF {transformToMote(match.defensaVic)}</h4>
+                <h4>DEF {transformToMote(match.defensaDerr)}</h4>
+                <h4>DEL {transformToMote(match.delanteroVic)}</h4>
+                <h4>DEL {transformToMote(match.delanteroDerr)}</h4>
+                <h2 className='elo_vic'>+{match.eloObtenido}</h2>
+                <h2 className='elo_derr'>-{match.eloObtenido}</h2>
+              </div>
             </div>
-            <div className='match_players'>
-              <h4>DF: {match.defensaVic}</h4>
-              <h4>DF: {match.defensaDerr}</h4>
-              <h4>DL: {match.delanteroVic}</h4>
-              <h4>DL: {match.delanteroDerr}</h4>
-              <h2 className='elo_vic'>+{match.eloObtenido}</h2>
-              <h2 className='elo_derr'>-{match.eloObtenido}</h2>
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
-      {/* TODO: Desplegar componentes <Match /> */}
+      {/* TODO: Render Match components */}
     </div>
   )
 }
